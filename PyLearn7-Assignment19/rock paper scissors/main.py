4ba8b5bae1b1c704f35f4495b7b9a59f60cbde75
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-02-06 00:48:22 +0330

    3rd commit

--- PyLearn7-Assignment19/rock paper scissors/main.py
+++ PyLearn7-Assignment19/rock paper scissors/main.py
@@ -11,16 +11,22 @@ score_cpu = 0
 
 def play(a):
     if a == "rock":
-        main_window.player_board.setIcon(QIcon("images.png"))
+        main_window.player_board.setIcon(QIcon("pics/rock_100.png"))
     elif a == "paper":
-        ...
+        main_window.player_board.setIcon(QIcon("pics/paper_100.png"))
     elif a == "scissors":
-        ...
+        main_window.player_board.setIcon(QIcon("pics/scissors_100.png"))
 
     cpu_play()
 
 def cpu_play():
-    ...
+    n = random.randint(1,3)
+    if n == 1:
+        main_window.cpu_board.setIcon(QIcon("pics/rock_101.png"))
+    elif n == 2:
+        main_window.cpu_board.setIcon(QIcon("pics/paper_101.png"))
+    elif n == 3:
+        main_window.cpu_board.setIcon(QIcon("pics/scissors_101.png"))
 
 
 
@@ -31,9 +37,12 @@ main_window = loader.load("main_window.ui")
 main_window.setWindowTitle("rock paper scissors")
 main_window.show()
 
-main_window.score_player.setText(str(score_player))
-main_window.score_cpu.setText(str(score_cpu))
+# main_window.score_player.setText(str(score_player))
+# main_window.score_cpu.setText(str(score_cpu))
 
+main_window.btn_rock.setIcon(QIcon("pics/rock_100.png"))
+main_window.btn_paper.setIcon(QIcon("pics/paper_100.png"))
+main_window.btn_scissors.setIcon(QIcon("pics/scissors_100.png"))
 main_window.btn_rock.clicked.connect(partial(play, "rock"))
 main_window.btn_paper.clicked.connect(partial(play, "paper"))
 main_window.btn_scissors.clicked.connect(partial(play, "scissors"))
