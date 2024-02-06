53f22e8e7b6a45465e66300007d4e85c6093ee01
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-02-07 00:06:48 +0330

    6th commit

--- PyLearn7-Assignment19/guess number/main_new.py
+++ PyLearn7-Assignment19/guess number/main_new.py
@@ -5,21 +5,34 @@ from PySide6.QtWidgets import QApplication
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtGui import *
 
-def play():
-    # gussed_num = int(main_window.txtbox.text())
-    check()
+number = random.randint(10,100)
+count = 0
+
+
 
 def check():
-    guessed_num = int(main_window.txtbox.text())
-    num = random.randint(10,100)
-    if guessed_num > num:
-        print("come down")
-        print(num)
-    elif guessed_num < num:
-        print("go up")
-        print(num)
-    elif guessed_num == num:
-        print("bingo")
+    global count
+    while True:
+        guessed_number = int(main_window.txtbox.text())
+        count = count +1
+
+        if number == guessed_number:
+            main_window.result_board.setText("Congratulations, you win!")
+            main_window.result_board_2.setText("You succeeded after")
+            main_window.result_board_2_1.setText(str(count))
+            main_window.result_board_2_2.setText("tries")
+            main_window.result_board_3.setIcon(QIcon("icons8-congratulations-100.png"))
+            
+            break
+
+        elif number > guessed_number:
+            main_window.result_board.setText("Go up. ⬆")
+            print(number)
+            break
+        elif number < guessed_number:
+            main_window.result_board.setText("Go down. ⬇")
+            print(number)
+            break
 
 
 loader = QUiLoader()
