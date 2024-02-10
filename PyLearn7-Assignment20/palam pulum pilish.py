9d4b0c262991f9608271ad932afc9889e33a3c98
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-02-10 15:46:50 +0330

    5th commit

--- PyLearn7-Assignment20/palam pulum pilish.py
+++ PyLearn7-Assignment20/palam pulum pilish.py
@@ -1,7 +1,7 @@
 import sys
 import random
 from functools import partial
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QMessageBox
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtGui import *
 
@@ -19,7 +19,8 @@ def play(player_choice):
     cpu1_play()
     cpu2_play()
     check(player_choice, cpu1_choice, cpu2_choice)
-    winer()
+    # winner()
+    
 
 def cpu1_play():
 
@@ -59,24 +60,57 @@ def check(player_choice, cpu1_choice, cpu2_choice):
         main_window.score_cpu2.setText(str(score_cpu2))
         main_window.result_board.setIcon(QIcon("pics\icons8-arrow-96 (2).png"))
 
-def winer():
+    winner()
+
+
+def winner():
     if score_player == 5:
-        ...
+        msg_box = QMessageBox(text = "Player wins!")
+        msg_box.exec()
+        # score_player = 0
+        new_game()
     elif score_cpu1 == 5:
-        ...
+        msg_box = QMessageBox(text = "Computer 1 wins!")
+        msg_box.exec()
+        # score_cpu1 = 0
+        new_game()
     elif score_cpu2 == 5:
-        ...
+        msg_box = QMessageBox(text = "Computer 2 wins!")
+        msg_box.exec()
+        # score_cpu2 = 0
+        new_game()
+
+    # new_game()
+
+def new_game():
+    
+    global score_player
+    global score_cpu1
+    global score_cpu2
+    score_player = score_cpu1 = score_cpu2 = 0
+    main_window.score_player.setText(str(score_player))
+    main_window.score_cpu1.setText(str(score_cpu1))
+    main_window.score_cpu2.setText(str(score_cpu2))
+    main_window.result_board.setIcon(QIcon(""))
+    main_window.player_board.setIcon(QIcon("pics\icons8-lotus-96.png"))
+    main_window.cpu1_board.setIcon(QIcon("pics\icons8-lotus-96.png"))
+    main_window.cpu2_board.setIcon(QIcon("pics\icons8-lotus-96.png"))
 
 
 loader = QUiLoader()
 app = QApplication(sys.argv)
 
 main_window = loader.load("main_window.ui")
-main_window.setWindowTitle("Palam Pulum Pilish")
+main_window.setWindowTitle("Lotus Palam-Pulum-Pilish")
 main_window.show()
 
 main_window.btn_front_hand.setIcon(QIcon("pics/front-hand-player-48.png"))
 main_window.btn_back_hand.setIcon(QIcon("pics/back-hand-player-48.png"))
+
+main_window.player_board.setIcon(QIcon("pics\icons8-lotus-96.png"))
+main_window.cpu1_board.setIcon(QIcon("pics\icons8-lotus-96.png"))
+main_window.cpu2_board.setIcon(QIcon("pics\icons8-lotus-96.png"))
+
 
 main_window.btn_front_hand.clicked.connect(partial(play, 1))
 main_window.btn_back_hand.clicked.connect(partial(play, 2))
