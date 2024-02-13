d6d13376ea881a63bc35042cebd4db71c86e897e
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-02-13 23:09:16 +0330

    2nd commit

--- PyLearn7-Assignment21/main.py
+++ PyLearn7-Assignment21/main.py
@@ -1,22 +1,61 @@
 import sqlite3
 
-connection = sqlite3.connect("chinook.db")
-my_cursor = connection.cursor()
 
-# for data in my_cursor.execute("SELECT * FROM customers WHERE Country = 'France'"):
-    # print(data)
+def show_menu():
+    print("1. show list")
+    print("2. add new")
+    print("3. edit")
+    print("4. remove")
+    print("5. search")
+    print("6. buy")
+    print("7. qr code")
+    print("8. exit")
 
-result = my_cursor.execute("SELECT * FROM customers WHERE Country = 'France'")
-french_customers = result.fetchall()
+def load_database():
+    global connection
+    global my_cursor
+    connection = sqlite3.connect("store.db")
+    my_cursor = connection.cursor()
 
-for customer in french_customers:
-    print(customer)
+def show_list():
+    # for data in my_cursor.execute("SELECT * FROM customers WHERE Country = 'France'"):
+        # print(data)
 
-# my_cursor.execute("INSERT INTO genres (GenreId, Name) VALUES (26, 'Traditional')")
-# connection.commit()
+    result = my_cursor.execute("SELECT * FROM products")
+    products = result.fetchall()
 
-# my_cursor.execute("UPDATE customers SET City='Mashhad', Country='Iran' WHERE FirstName='Helena' AND CustomerId=6")
-# connection.commit()
+    for product in products:
+        print(product)
 
-my_cursor.execute("DELETE FROM artists WHERE ArtistId=10")
-connection.commit()
+def add_new():
+    new_product_name = input("Enter your new product name: ")
+    my_cursor.execute(f"INSERT INTO products (Name) VALUES ('{new_product_name}')")
+    connection.commit()
+
+def edit():
+    product_id = input("Enter the product id: ")
+    product_name = input("Enter the product name: ")
+    new_price = input("Enter the new price: ")
+    my_cursor.execute(f"UPDATE products SET Price='{new_price}' WHERE ProductId='{product_id}' AND Name='{product_name}'")
+    connection.commit()
+
+def remove():
+    product_id = input("Enter the product id: ")
+    product_name = input("Enter the product name: ")
+    my_cursor.execute(f"DELETE FROM products WHERE ProductId = '{product_id}' AND Name = '{product_name}'")
+    connection.commit()
+
+
+load_database()
+while True:
+    show_menu()
+    choice = int(input())
+
+    if choice == 1:
+        show_list()
+    elif choice == 2:
+        add_new()
+    elif choice == 3:
+        edit()
+    elif choice == 4:
+        remove()
--- PyLearn7-Assignment21/test.py
+++
@@ -1,8 +0,0 @@
-name = "Mahdi"
-family = "Ahmadi Kamal"
-age = 32
-
-print("Hello! I am", name, family,". I am", age, "years old.")
-print(f"Hello! I am {name} {family}. I am {age} years old.")
-
-
