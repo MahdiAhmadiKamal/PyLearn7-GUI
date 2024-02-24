632baefeabc1e6de14da6809ae3ed865662cd6ac
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-02-24 23:47:22 +0330

    2nd commit

--- PyLearn7-Assignment23/main.py
+++ PyLearn7-Assignment23/main.py
@@ -1,4 +1,5 @@
 import sys
+from sudoku import Sudoku
 from PySide6.QtWidgets import *
 from main_window import Ui_MainWindow
 
@@ -9,9 +10,14 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        puzzle = Sudoku(3).difficulty(0.5)
+        print(puzzle.board)
+
         for i in range(9):
             for j in range(9):
                 new_cell = QLineEdit()
+                if puzzle.board[i][j] != None:
+                    new_cell.setText(str(puzzle.board[i][j]))
                 self.ui.grid_layout.addWidget(new_cell, i, j)
 
 if __name__ == "__main__":
