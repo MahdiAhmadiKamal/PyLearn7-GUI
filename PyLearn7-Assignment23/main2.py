43e7daf605b5a03097c2fa9e9665b4eae5194df1
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-02-26 15:06:15 +0330

    4th commit

--- PyLearn7-Assignment23/main2.py
+++ PyLearn7-Assignment23/main2.py
@@ -3,6 +3,8 @@ import random
 from functools import partial
 from sudoku import Sudoku
 from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6 import QtWidgets, QtCore
 from main_window import Ui_MainWindow
 
 
@@ -14,25 +16,29 @@ class MainWindow(QMainWindow):
         self.ui.menu_new.triggered.connect(self.new_game)
         self.ui.menu_open_file.triggered.connect(self.open_file)
         self.line_edits = [[None for i in range(9)] for j in range(9)]
-        for i in range(9):
-            for j in range(9):
-                new_cell = QLineEdit()
-                self.ui.grid_layout.addWidget(new_cell, i, j)
-                new_cell.textChanged.connect(partial(self.validation, i, j))
-                self.line_edits[i][j] = new_cell
-
         self.new_game()
        
     def new_game(self):
         puzzle = Sudoku(3, seed=random.randint(1, 1000)).difficulty(0.5)
         for i in range(9):
             for j in range(9):
-                self.line_edits[i][j].setReadOnly(False)    #It was not necessary to write this line in code main2
+                new_cell = QLineEdit()
+                self.appearance(new_cell)
+
                 if puzzle.board[i][j] != None:
-                    self.line_edits[i][j].setText(str(puzzle.board[i][j]))
-                    self.line_edits[i][j].setReadOnly(True)
-                else:
-                    self.line_edits[i][j].setText("")
+                    new_cell.setText(str(puzzle.board[i][j]))
+                    new_cell.setReadOnly(True)
+                self.ui.grid_layout.addWidget(new_cell, i, j)
+                
+                new_cell.textChanged.connect(partial(self.validation, i, j))
+                self.line_edits[i][j] = new_cell
+
+    def appearance(self, cell):
+        cell.setAlignment(QtCore.Qt.AlignCenter)
+        cell.setFont(QFont("Segoe UI Black", 10))
+        
+        
+        
 
     def open_file(self):
         file_path = QFileDialog.getOpenFileName(self, "Open file...")[0]
@@ -47,12 +53,14 @@ class MainWindow(QMainWindow):
 
         for i in range(9):
             for j in range(9):
-                self.line_edits[i][j].setReadOnly(False)    #It was not necessary to write this line in code main2
+                new_cell = QLineEdit()
+                self.appearance(new_cell)
                 if puzzle_board[i][j] != 0:
-                    self.line_edits[i][j].setText(str(puzzle_board[i][j]))
-                    self.line_edits[i][j].setReadOnly(True)
-                else:
-                    self.line_edits[i][j].setText("")
+                    new_cell.setText(str(puzzle_board[i][j]))
+                    new_cell.setReadOnly(True)
+                self.ui.grid_layout.addWidget(new_cell, i, j)
+                new_cell.textChanged.connect(partial(self.validation, i, j))
+                self.line_edits[i][j] = new_cell
 
     def check(self):
         for i in range(0, 9):
@@ -62,14 +70,15 @@ class MainWindow(QMainWindow):
                 if number1 == number2:
                     print("❌")
                     return False
-
+        
     def validation(self, i, j, text):
         # text = self.line_edits[i][j].text()
         if text not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
             self.line_edits[i][j].setText("")
-        
+
         if self.check() == True:
             msg_box = ...
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
---
+++ PyLearn7-Assignment23/test.py
@@ -0,0 +1,2 @@
+w =[[None for i in range(4)] for j in range(3)]
+print(w)
