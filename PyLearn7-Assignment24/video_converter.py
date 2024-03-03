0891cc3a313ec500855523fb8ec0efb9f8ac5b45
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-03-03 15:07:44 +0330

    1st commit

---
+++ PyLearn7-Assignment24/video_converter.py
@@ -0,0 +1,15 @@
+from moviepy import editor
+
+def convert(input_address,output_address):
+    video = editor.VideoFileClip(input_address)
+    video.audio.write_audiofile(output_address)
+
+
+movies = [['input/video1.mp4', 'output/audio1.mp3'],
+          ['input/video2.mp4', 'output/audio2.mp3'],
+          ['input/video3.mp4', 'output/audio3.mp3'],
+          ['input/video4.mp4', 'output/audio4.mp3'],
+          ['input/video5.mp4', 'output/audio5.mp3']]
+
+for movie in movies:
+    convert(movie[0], movie[1] )
