b6bba8da04f5e7c370a76df35a31c77d405332db
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-03-06 10:52:58 +0330

    2nd commit

--- PyLearn7-Assignment25/main.py
+++ PyLearn7-Assignment25/main.py
@@ -2,11 +2,14 @@ import sys
 import time
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
-from PySide6.QtCore import QThread
+from PySide6.QtCore import QThread, Signal
 from PySide6.QtUiTools import QUiLoader
 
 
 class MyThread(QThread):
+
+    my_signal = Signal(int)
+
     def __init__(self):
         super().__init__()
         self.second = 0
@@ -14,11 +17,15 @@ class MyThread(QThread):
     def run(self):
         while True:
             self.second += 1
-            print(self.second)
+            # print(self.second)
+            self.my_signal.emit(self.second)
             time.sleep(1)
 
 def start_stopwatch():
     thread_stopwatch.start()
+
+def show_number(s):
+    window.lbl_stopwatch.setText(str(s))
 
 if __name__ == "__main__":
     
@@ -30,6 +37,6 @@ if __name__ == "__main__":
 
     thread_stopwatch = MyThread()
     window.btn_start_stopwatch.clicked.connect(start_stopwatch)
+    thread_stopwatch.my_signal.connect(show_number)
 
     app.exec()
-
